646a9b1137a88b00905c4534e4409eb92a739792
lihaoliang <cvbmgh@126.com>
2017-08-04 19:08:22 +0800

    first commit

---
+++ frontend/controllers/YiiGridController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace frontend\controllers;
+use yii\web\Controller;
+
+/**
+ * Description of YiiGridController
+ *
+ * @author Administrator
+ */
+class YiiGridController extends Controller{
+    
+    public function  actionIndex(){
+        
+    }
+}
